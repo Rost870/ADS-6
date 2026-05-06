cc0b6745d1e5a9295eb920771f003dc6106e096c
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-05-06 16:44:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 // Copyright 2026 <Your Name>
 #include "tpqueue.h"
 
-struct SYM {
-    char ch;
-    int prior;
-};
-
 int main() {
     TPQueue<SYM> pqueue;
     pqueue.push(SYM{'a', 4});
     pqueue.push(SYM{'b', 7});
     SYM c1 = pqueue.pop();
     SYM c2 = pqueue.pop();
+    (void)c1;
+    (void)c2;
     return 0;
 }
